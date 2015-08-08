a17915d08d664acbfb45d923093b997445c7132d
maticaputti <matias.caputti@gmail.com>
2015-08-08 20:29:11 -0300

    Rendering a list of Articles

--- app.js
+++ app.js
@@ -11,21 +11,74 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require("angular2/angular2");
-var HelloWorld = (function () {
-    function HelloWorld() {
+var HeaderApp = (function () {
+    function HeaderApp() {
         this.name = 'Matias';
         this.names = ['Pepe', 'Juan', 'Pedro'];
     }
-    HelloWorld = __decorate([
+    HeaderApp = __decorate([
         angular2_1.Component({
-            selector: 'hello-world'
+            selector: 'test-header'
         }),
         angular2_1.View({
             directives: [angular2_1.NgFor],
-            template: "\n        <div>\n            <article>\n                Hello {{ name }}\n            </article>\n        </div>\n        <div>\n            <ul>\n                <li *ng-for=\"#name of names\">Hello {{ name }}</li>\n            </ul>\n        </div>\n    "
+            template: "\n        <div class=\"header\">\n            <div>\n                <article>\n                    Hello {{ name }}\n                </article>\n            </div>\n            <div>\n                <ul class=\"list\">\n                    <li class=\"list-item\" *ng-for=\"#name of names\">Hello {{ name }}</li>\n                </ul>\n            </div>\n        </div>\n    "
         }), 
         __metadata('design:paramtypes', [])
-    ], HelloWorld);
-    return HelloWorld;
+    ], HeaderApp);
+    return HeaderApp;
 })();
-angular2_1.bootstrap(HelloWorld);
+var Article = (function () {
+    function Article(title, link) {
+        this.title = title;
+        this.link = link;
+        this.votes = 0;
+    }
+    Article.prototype.voteUp = function () {
+        this.votes += 1;
+        return false;
+    };
+    Article.prototype.voteDown = function () {
+        this.votes -= 1;
+        return false;
+    };
+    return Article;
+})();
+var ArticleApp = (function () {
+    function ArticleApp() {
+    }
+    ArticleApp = __decorate([
+        angular2_1.Component({
+            selector: 'test-article',
+            properties: ['article'],
+        }),
+        angular2_1.View({
+            template: "\n        <article>\n            <div class=\"votes\">{{ article.votes }}</div>\n            <div class=\"main\">\n                <h2>\n                    <a href=\"{{ article.link }}\">{{ article.title }}</a>\n                </h2>\n                <ul>\n                    <li>\n                        <a href (click)='article.voteUp()'>upvote</a>\n                    </li>\n                    <li>\n                        <a href (click)='article.voteDown()'>downvote</a>\n                    </li>\n                </ul>\n            </div>\n        </article>\n    "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ArticleApp);
+    return ArticleApp;
+})();
+var ConsoleApp = (function () {
+    function ConsoleApp() {
+        this.articles = [
+            new Article('Angular 2', 'http://angular.io'),
+            new Article('Fullstack', 'http://fullstack.io')
+        ];
+    }
+    ConsoleApp.prototype.addArticle = function (title, link) {
+        console.log("Adding article with title", title.value, "and link", link.value);
+    };
+    ConsoleApp = __decorate([
+        angular2_1.Component({
+            selector: 'test-console'
+        }),
+        angular2_1.View({
+            directives: [angular2_1.NgFor, ArticleApp, HeaderApp],
+            template: "\n        <test-header></test-header>\n\n        <section class=\"new-link\">\n            <div class=\"control-group\">\n                <div><label for=\"title\">Title:</label></div>\n                <div><input name=\"title\" #newtitle></div>\n            </div>\n            <div class=\"control-group\">\n                <div><label for=\"link\">Link:</label></div>\n                <div><input name=\"link\" #newlink></div>\n            </div>\n\n            <button (click)=\"addArticle(newtitle, newlink)\">Submit Link</button>\n        </section>\n\n        <test-article *ng-for=\"#article of articles\" [article]=\"article\">{{ article }}</test-article>\n    "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ConsoleApp);
+    return ConsoleApp;
+})();
+angular2_1.bootstrap(ConsoleApp);
--- app.ts
+++ app.ts
@@ -7,27 +7,31 @@ import {
     bootstrap,
 } from "angular2/angular2";
 
+
+// Header component
 @Component({
-    selector: 'hello-world'
+    selector: 'test-header'
 })
 
 @View({
     directives: [NgFor],
     template: `
-        <div>
-            <article>
-                Hello {{ name }}
-            </article>
-        </div>
-        <div>
-            <ul>
-                <li *ng-for="#name of names">Hello {{ name }}</li>
-            </ul>
+        <div class="header">
+            <div>
+                <article>
+                    Hello {{ name }}
+                </article>
+            </div>
+            <div>
+                <ul class="list">
+                    <li class="list-item" *ng-for="#name of names">Hello {{ name }}</li>
+                </ul>
+            </div>
         </div>
     `
 })
 
-class HelloWorld {
+class HeaderApp {
     name: string;
     names: Array<string>;
 
@@ -37,4 +41,106 @@ class HelloWorld {
     }
 }
 
-bootstrap(HelloWorld);
+class Article {
+    title: string;
+    link: string;
+    votes: number;
+
+    constructor(title, link) {
+        this.title = title;
+        this.link = link;
+        this.votes = 0;
+    }
+
+    voteUp() {
+        this.votes += 1;
+        return false;
+        // Javascript, by default, propagates the click event to all
+        // the parent components. Becouse the click event is propagated
+        // to parents, our browser is trying to follow an empty link.
+        // To fix that, we just need to make event handler to return false.
+    }
+
+    voteDown() {
+        this.votes -= 1;
+        return false;
+    }
+}
+
+// Article component
+@Component({
+    selector: 'test-article',
+    properties: ['article'],
+})
+
+@View({
+    template: `
+        <article>
+            <div class="votes">{{ article.votes }}</div>
+            <div class="main">
+                <h2>
+                    <a href="{{ article.link }}">{{ article.title }}</a>
+                </h2>
+                <ul>
+                    <li>
+                        <a href (click)='article.voteUp()'>upvote</a>
+                    </li>
+                    <li>
+                        <a href (click)='article.voteDown()'>downvote</a>
+                    </li>
+                </ul>
+            </div>
+        </article>
+    `
+})
+
+class ArticleApp {
+    article: Article;
+}
+
+
+// Console component
+@Component({
+    selector: 'test-console'
+})
+
+@View({
+    directives: [NgFor, ArticleApp, HeaderApp],
+    template: `
+        <test-header></test-header>
+
+        <section class="new-link">
+            <div class="control-group">
+                <div><label for="title">Title:</label></div>
+                <div><input name="title" #newtitle></div>
+            </div>
+            <div class="control-group">
+                <div><label for="link">Link:</label></div>
+                <div><input name="link" #newlink></div>
+            </div>
+
+            <button (click)="addArticle(newtitle, newlink)">Submit Link</button>
+        </section>
+
+        <test-article *ng-for="#article of articles" [article]="article">{{ article }}</test-article>
+    `
+})
+
+class ConsoleApp {
+    articles: Array<Article>;
+
+    constructor() {
+        this.articles = [
+            new Article('Angular 2', 'http://angular.io'),
+            new Article('Fullstack', 'http://fullstack.io')
+        ];
+    }
+
+    addArticle(title, link) {
+        console.log("Adding article with title", title.value, "and link", link.value);
+    }
+}
+
+
+// bootstraping
+bootstrap(ConsoleApp);
